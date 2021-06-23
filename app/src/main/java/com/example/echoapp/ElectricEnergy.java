af743a687ee7ef08d468cac9c78890de5f53c53d
bismaysarao <bismaysinghsarao@gmail.com>
2021-06-23 18:19:29 +0200

    Bismay update

--- app/src/main/java/com/example/echoapp/ElectricEnergy.java
+++ app/src/main/java/com/example/echoapp/ElectricEnergy.java
@@ -2,17 +2,13 @@ package com.example.echoapp;
 
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ElectricEnergy extends AppCompatActivity {
-
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.electric_energy);
+
     }
-
-
 }
--- app/src/main/java/com/example/echoapp/GasEnergy.java
+++ app/src/main/java/com/example/echoapp/GasEnergy.java
@@ -1,12 +1,11 @@
 package com.example.echoapp;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class GasEnergy extends AppCompatActivity{
+public class GasEnergy extends AppCompatActivity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gas_energy);
     }
